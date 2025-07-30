a8acd9b966ea710937e39acf9a91a4aabdb96355
liminyu <lukmin@foxmail.com>
2025-07-30 11:33:53 +0800

    desc ver
    numerical equilibrium version

---
+++ code_num_gkmsol1.0_1_num_itpa/initial_mod.f90
@@ -0,0 +1,43 @@
+          module initial_mod
+           contains
+
+            subroutine ini
+             use vars
+             use vars1
+             use global
+             use vars_k
+             use vars_e
+             use inrtype
+             use splines
+             use supralu_mod
+             use orbit_eq_mod
+             ! generate mesh_xal_arc.dat of particle mesh
+             ! with calling check_mesh3
+             !  
+!            equilibrium for device configuration
+             call check_eq ! using gfile, bkg, rmaj input
+!            parameters for device and particles
+             call profiledat
+             call tensordat
+             call profcoefdat
+             call setgrid
+!            flux surface making for gfile
+!             call contourpoints
+!             call contourpointsr
+!             coefficients of Fouier components of R and Z with straight
+!            field line coordinates of flux
+!             call tensordat
+!            flux surface recording
+             !call check4
+             !call check_mesh
+             !call check_mesh1
+             call check_flux1
+             !call check_flux2
+            ! call check_mesh2
+            ! call check_mesh3
+             call check4
+             call delx
+             call check_mesh5
+             !call check_mesh4
+           end subroutine ini
+          end module initial_mod
